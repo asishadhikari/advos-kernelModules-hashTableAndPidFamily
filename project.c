d15185ffc6fc2a620b9d0fa37fa4dfa5f38ee97a
sikari <ashish.adhikari@bison.howard.edu>
2018-02-05 21:45:03 -0500

    preliminary

--- project.c
+++ project.c
@@ -1,8 +1,35 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+
+struct birthday{
+	int day;
+	int month;
+	int year;
+	//list_head has next and prev
+	struct list_head list;
+};
+
+//macro to init birthday_list of type LIST_HEAD
+static LIST_HEAD(birthday_list);
+
+
+
+
+
+
+
+
 /* This function is called when the module is loaded. */
 int simple init(void){
+	struct birthday *person;
+	person = kmalloc(sizeof(*person),GFP_KERNEL);
+	person->day = 23;
+	person->month = 6;
+	person->year = 1994;
+	//initialise the list head with first node's list
+	INIT_LIST_HEAD(&person->list);
+
 	printk(KERN INFO "Loading Module\n");
 	return 0;
 }
